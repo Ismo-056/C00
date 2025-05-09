bf7743f971554303da204a875ca8d4d06d9022de
MarliansLife <hassanismael056@gmail.com>
2025-05-09 22:13:39 +0100

    C00

--- current_datetime.sh
+++ current_datetime.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ceci fait affiché la date et l'heure actuel
-echo "La date et l'heure actuelles sont : $(date '+%Y-%m-%d %H:%M:S')"
+echo "La date et l'heure actuelles sont : $(date)"
 
 
 
