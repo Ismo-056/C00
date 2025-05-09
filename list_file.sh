2ab6673035262fb19075447c829550e1ff15c080
MarliansLife <hassanismael056@gmail.com>
2025-05-09 21:34:15 +0100

    C00

--- list_file.sh
+++ list_file.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Ce script liste tous les fichiers du répertoire courant avec des détails
 echo "Liste des fichiers du répertoire courant :" ls -lh --ignore='*/'
-chmod +x list_file.sh
 
