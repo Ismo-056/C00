3a3a319a3030ecfcc6681c75032a24e7661aa3b4
MarliansLife <hassanismael056@gmail.com>
2025-05-10 08:44:32 +0100

    C00

--- current_datetime.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# ceci fait affiché la date et l'heure actuel
-echo "La date et l'heure actuelles sont : $(date '+%Y-%m-%d %H:%M:%S')"
-
-
-
-
-
-
--- hello_world.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# ce script fait affiché hello_world à l'ecran
-echo "Hello, world!"
-
