be74c2e32c82799ef60dc240064ff8f4da31adfb
MarliansLife <hassanismael056@gmail.com>
2025-05-11 10:56:06 +0100

    C00

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -3,7 +3,7 @@
 filename="Sample.txt"
 echo "Donnez le nom du fichier"
  read filename
-if [ -f "$filename" ]; then
+if [ -f "$filename" ];then
  echo "Le fichier '$filename' existe."
 else
  echo "Le fichier '$filename' n'existe pas."
