1e7230f74c46923e67d1143c79ac6aa34b105950
MarliansLife <hassanismael056@gmail.com>
2025-05-12 11:26:21 +0100

    C00

---
+++ create_directory.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Ce script crée un répertoire nommé "test_directory"
+mkdir test_directory
+echo "le répertoire 'test_directory' a été créé."
---
+++ hello_world.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Ce script affiche le texte "Hello, World!" sur le terminal
+echo "Hello, World!"
+
